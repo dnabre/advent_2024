dbe8ad2d0792ef31d7b84425affc4d91229ab1f5
dnabre <dnabre@happybunnylabs.com>
2024-12-02 08:59:21 -0700

    Day 02, part 1 done

--- src/main/java/aoc_2024/AdventOfCode2024.java
+++ src/main/java/aoc_2024/AdventOfCode2024.java
@@ -1,20 +1,22 @@
 package src.main.java.aoc_2024;
 
 
+import java.io.IOException;
+
 public class AdventOfCode2024 {
     public static void main(String[] args) {
 
       //  System.out.println("Working Directory = " + System.getProperty("user.dir"));
 
-        String test_01_string = "input/2024/day_01_test_01.txt";
-        String input_string = "input/2024/day_01_input_01.txt";
+       // String input_string = "input/2024/day_02_test_01.txt";
+        String input_string = "input/2024/day_02_input_01.txt";
         try {
-            String[] results = Day01.runDay(System.out,input_string );
-            //String[] results = Day01.runDay(System.out,test_01_string );
+            String[] results = Day02.runDay(System.out,input_string );
+
 
             System.out.printf("\t\tpart1:\t\t%s\n", results[0]);
             System.out.printf("\t\tpart2:\t\t%s\n", results[1]);
-        } catch (Exception e) {
+        } catch (IOException e) {
             throw new RuntimeException("error opening file " + input_string + " " + e.toString());
         }
 
