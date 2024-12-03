9f300f106372516bc4e8e148436bbb48472170b0
dnabre <dnabre@happybunnylabs.com>
2024-12-03 09:43:29 -0700

    Day 03
    parts 1 & 2 done
    clean-up finished

--- src/main/java/aoc_2024/Day03.java
+++ src/main/java/aoc_2024/Day03.java
@@ -8,17 +8,18 @@ import java.nio.file.Path;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import static java.lang.System.out;
-
 
 public class Day03 {
     public static final String PART1_ANSWER = "161085926";
-    public static final String PART2_ANSWER = "476";// too low
+    public static final String PART2_ANSWER = "82045421";// too low
 
     public static final String PART1_REGEX = "mul\\(\\d{1,3},\\d{1,3}\\)";
+    public static final String PART2_REGEX = "mul\\(\\d{1,3},\\d{1,3}\\)|don\\'t\\(\\)|do\\(\\)";
+
+
     public static String[] runDay(PrintStream out, String inputString) throws IOException {
-//        out.println("Advent of Code 2024");
-//        out.println("\tDay  3");
+        out.println("Advent of Code 2024");
+        out.println("\tDay  3");
 
 
         String[] answers = {"", ""};
@@ -26,11 +27,11 @@ public class Day03 {
         answers[1] = getPart2(inputString);
 
 
-        if (!answers[0].equals(Day02.PART1_ANSWER)) {
+        if (!answers[0].equals(PART1_ANSWER)) {
             out.printf("\t\tWRONG ANSWER got: %s, expected %s\n", answers[0], Day02.PART1_ANSWER);
         }
 
-        if (!answers[1].equals(Day02.PART2_ANSWER)) {
+        if (!answers[1].equals(PART2_ANSWER)) {
             out.printf("\n\t\tWRONG ANSWER got: %s, expected %s\n", answers[1], Day02.PART2_ANSWER);
         }
         return answers;
@@ -38,37 +39,48 @@ public class Day03 {
 
     public static String getPart1(String s) throws IOException {
         String input = Files.readString(Path.of(s.trim()));
-        out.printf("read %d characters\n", input.length());
-
 
         Pattern pat = Pattern.compile(PART1_REGEX);
         Matcher matcher = pat.matcher(input);
-        int match_count =0;
-        int total =0;
+        int total = 0;
 
         while (matcher.find()) {
             String mg = matcher.group();
-            String nums = mg.substring(4, mg.length()-1);
+            String nums = mg.substring(4, mg.length() - 1);
             String[] parts = nums.split(",");
             int left = Integer.parseInt(parts[0]);
             int right = Integer.parseInt(parts[1]);
-            total += left*right;
-            match_count++;
+            total += left * right;
         }
-
-
-        out.printf("match_count: %d\n", match_count);
-
-
         int answer = total;
         return Integer.toString(answer);
     }
 
     public static String getPart2(String s) throws IOException {
-        String[] lines = Files.readAllLines(Path.of(s.trim())).toArray(new String[0]);
-        int answer = 2;
+        String input = Files.readString(Path.of(s.trim()));
+
+        Pattern pat = Pattern.compile(PART2_REGEX);
+        Matcher matcher = pat.matcher(input);
+        int total = 0;
+        boolean mul_enabled = true;
+
+        while (matcher.find()) {
+            String mg = matcher.group();
+            if (mg.startsWith("mul")) {
+                if (mul_enabled) {
+                    String nums = mg.substring(4, mg.length() - 1);
+                    String[] parts = nums.split(",");
+                    int left = Integer.parseInt(parts[0]);
+                    int right = Integer.parseInt(parts[1]);
+                    total += left * right;
+                }
+            } else if (mg.equals("don't()")) {
+                mul_enabled = false;
+            } else {
+                mul_enabled = true;
+            }
+        }
+        int answer = total;
         return Integer.toString(answer);
     }
-
-
 }
