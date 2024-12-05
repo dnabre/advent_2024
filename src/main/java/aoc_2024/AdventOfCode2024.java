db3cd403e3835245d2d431c96d16e864de6918a8
dnabre <dnabre@happybunnylabs.com>
2024-12-05 12:07:18 -0700

    Day 05  both parts done  cleaned up

--- src/main/java/aoc_2024/AdventOfCode2024.java
+++ src/main/java/aoc_2024/AdventOfCode2024.java
@@ -4,13 +4,39 @@ package src.main.java.aoc_2024;
 import java.io.IOException;
 
 public class AdventOfCode2024 {
-    public static void main(String[] args) {
-       // String input_string = "input/2024/day_05_test_01.txt";
-        String input_string = "input/2024/day_05_input_01.txt";
+    static final String[] test_files = {
+            "input/2024/day_01_test_01.txt",
+            "input/2024/day_02_test_01.txt",
+            "input/2024/day_03_test_01.txt",
+            "input/2024/day_04_test_01.txt",
+            "input/2024/day_05_test_01.txt",
+    };
+
+    static final String[] input_files= {
+            "input/2024/day_01_input_01.txt",
+            "input/2024/day_02_input_01.txt",
+            "input/2024/day_03_input_01.txt",
+            "input/2024/day_04_input_01.txt",
+            "input/2024/day_05_input_01.txt",
+    };
+
+
+    public static final boolean TESTING = false;
+
+    public static void main(String[] args){
+
+        int day_number=5;
+
+
+        String input_string;
+        if (TESTING) {
+            input_string = test_files[day_number-1];
+        } else {
+            input_string = input_files[day_number-1];
+        }
+
         try {
             String[] results = Day05.runDay(System.out, input_string);
-
-
             System.out.printf("\t\tpart1:\t\t%s\n", results[0]);
             System.out.printf("\t\tpart2:\t\t%s\n", results[1]);
         } catch (IOException e) {
