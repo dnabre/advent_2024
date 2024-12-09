c68f427746b04b4540f5757881fa072e8aa2bc6e
dnabre <dnabre@happybunnylabs.com>
2024-12-09 16:10:12 -0700

    day 7 time

--- src/main/java/aoc_2024/AoCUtils.java
+++ src/main/java/aoc_2024/AoCUtils.java
@@ -3,13 +3,9 @@ package src.main.java.aoc_2024;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.List;
 
 
 public class AoCUtils {
-
-    public static char[] LABELS = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 
     public static int[] WhitespaceDelimitedLineToIntegers(String ln) {
         String[] parts = ln.split("\\s+");
@@ -48,35 +44,5 @@ public class AoCUtils {
         return grid;
     }
 
-    public static int[] charArrayToIntArray(char[] ch_array) {
-        int[] r = new int[ch_array.length];
-        for (int i = 0; i < ch_array.length; i++) {
-            r[i] = (int) ch_array[i];
-        }
-        return r;
-    }
-
-
-
-    public static List<Vector2d[]> getAllPairingsVector2d(List<Vector2d> antList) {
-        List<Vector2d[]> ls = new ArrayList<>();
-        Vector2d[] ant_array = antList.toArray(new Vector2d[0]);
-        for (int a = 0; a < ant_array.length; a++) {
-            for (int b = a + 1; b < ant_array.length; b++) {
-                Vector2d[] new_pair = new Vector2d[2];
-                new_pair[0] = ant_array[a];
-                new_pair[1] = ant_array[b];
-                ls.add(new_pair);
-            }
-        }
-        return ls;
-    }
-
-
-    public static void chSwap(char[] a, int i, int j) {
-        char t = a[i];
-        a[i] = a[j];
-        a[j] = t;
-    }
 
 }
