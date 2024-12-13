10154e9c9c0ced0a516f4a0849c6082076f8b977
M.A. MacLeay <dnabre@happybunnylabs.com>
2024-12-13 07:56:47 -0700

    Day 10 parts 1 & 2 both working cleanup needed

--- src/main/java/aoc_2024/Day10.java
+++ src/main/java/aoc_2024/Day10.java
@@ -71,18 +71,38 @@ public class Day10 {
         return String.valueOf(answer);
     }
 
+    public static String getPart2()  {
+        out.printf("max x: %d,  y: %d \n", grid[0].length,grid.length);
+        //    AoCUtils.printGrid(grid);
+
+        List<Vector2d> starts = getStartingPositions();
+        //       out.printf("starting positions: ");
+        for(Vector2d s:starts) {
+            //           out.printf("start: %s, %c \n", s, grid[s.x][s.y]);
+        }
+
+        List<Vector2d> targets = getTargetPositions();
+        //       out.printf("target positions: \t %s \n ", targets);
+
+        int path_total =0;
+        out.println();
+        for(Vector2d start: starts) {
+            int found_paths = FindDistinctPathStartToPeaks(start, targets);
+            path_total += found_paths;
+            //      out.printf("found %d paths starting from %s to %s\n", found_paths, start, targets);
+
+        }
+
+
+        long answer = path_total;
+        return String.valueOf(answer);
+    }
+
+
+
     private static int FindPathStartToPeaks(Vector2d start_point, List<Vector2d> targets) {
         HashSet<Vector2d> peak_pathed_to = new HashSet<>();
-        record State(Vector2d current){
-            public int height() {
-                return Character.getNumericValue(grid[current.x][current().y]);
-            }
 
-            @Override
-            public String toString() {
-                return String.format("@%s, height: %d", this.current, this.height());
-            }
-        }
         int min_idx = 0;
         int max_idx = grid.length -1;
         HashSet<State> seen = new HashSet<>();
@@ -93,7 +113,7 @@ public class Day10 {
         work_queue.addLast(start);
 
         while(!work_queue.isEmpty()){
-          //  out.printf("current state: %s, queue: %s \n", work_queue.getFirst(), work_queue);
+            //  out.printf("current state: %s, queue: %s \n", work_queue.getFirst(), work_queue);
             State c = work_queue.removeFirst();
             if (targets.contains(c.current)) {
                 peak_pathed_to.add(c.current);
@@ -115,12 +135,45 @@ public class Day10 {
         //out.printf("peaks found: %s\n", peak_pathed_to);
         return peak_pathed_to.size();
     }
+    private static int FindDistinctPathStartToPeaks(Vector2d start_point, List<Vector2d> targets) {
+        HashSet<ArrayList<Vector2d>> unique_paths = new HashSet<>();
 
-    public static String getPart2()  {
+        int min_idx = 0;
+        int max_idx = grid.length -1;
+        HashSet<State2> seen = new HashSet<>();
 
-        long answer = 2;
-        return String.valueOf(answer);
+        State2 start = new State2(start_point, new ArrayList<Vector2d>(10));
+        start.path.add(start_point);
+
+        ArrayDeque<State2> work_queue = new ArrayDeque<>();
+        work_queue.addLast(start);
+
+        while(!work_queue.isEmpty()){
+            //  out.printf("current state: %s, queue: %s \n", work_queue.getFirst(), work_queue);
+            State2 c = work_queue.removeFirst();
+            if (targets.contains(c.current)) {
+                unique_paths.add(c.path);
+            }
+
+            List<Vector2d> neighs = Directions.Compass.getNeighborsClamped(c.current,min_idx,max_idx);
+            for(Vector2d vv: neighs) {
+                if (Character.getNumericValue(grid[vv.x][vv.y]) == c.height() + 1) {
+                    ArrayList<Vector2d> path = new ArrayList<>(c.path);
+                    path.add(vv);
+                    State2 new_state = new State2(vv, path);
+                    if(!seen.contains(new_state)) {
+                        seen.add(new_state);
+                        work_queue.add(new_state);
+                    }
+                }
+            }
+
+        }
+
+        //out.printf("peaks found: %s\n", peak_pathed_to);
+        return unique_paths.size();
     }
+
 
     private static List<Vector2d> getStartingPositions() {
         List<Vector2d> pos = new ArrayList<>();
@@ -150,5 +203,24 @@ public class Day10 {
         }
         return pos;
     }
+    record State(Vector2d current){
+        public int height() {
+            return Character.getNumericValue(grid[current.x][current().y]);
+        }
 
+        @Override
+        public String toString() {
+            return String.format("@%s, height: %d", this.current, this.height());
+        }
+    }
+    record State2(Vector2d current, ArrayList<Vector2d> path){
+        public int height() {
+            return Character.getNumericValue(grid[current.x][current().y]);
+        }
+
+        @Override
+        public String toString() {
+            return String.format("@%s, height: %d (S2)", this.current, this.height());
+        }
+    }
 }
