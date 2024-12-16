be45c9732ab788d94fbf5723ca72c9b98b1b25e4
dnabre <dnabre@happybunnylabs.com>
2024-12-15 21:59:28 -0700

    Day 14 part 1 done. part 2 done

--- src/main/java/aoc_2024/Day14.java
+++ src/main/java/aoc_2024/Day14.java
@@ -5,8 +5,9 @@ import java.io.PrintStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
+
+import static java.lang.System.out;
 
 
 public class Day14 {
@@ -15,6 +16,7 @@ public class Day14 {
     public static final String PART2_ANSWER = "7687";
 
     public static final int PART1_TICKS = 100;
+    public static final int PART2_TICKS = Integer.valueOf(PART2_ANSWER);
     private static ArrayList<Robot> robot_initial;
 
     public static String[] runDay(PrintStream out, String inputString) throws IOException {
@@ -65,8 +67,7 @@ public class Day14 {
             Robot n_r = r.elapsed(PART1_TICKS, map_size);
             new_positions.add(n_r);
         }
-        HashSet<Robot> set = new HashSet<>();
-        set.addAll(new_positions);
+
 
         long quad_width = map_size.x / 2L;
         long quad_height = map_size.y / 2L;
@@ -92,6 +93,33 @@ public class Day14 {
     }
 
     public static String getPart2() {
+        Vector2d map_size = AdventOfCode2024.TESTING ? new Vector2d(11, 7) : new Vector2d(101, 103);
+        ArrayList<Robot> new_positions = new ArrayList<>();
+        for (Robot r : robot_initial) {
+            Robot n_r = r.elapsed(PART2_TICKS, map_size);
+            new_positions.add(n_r);
+        }
+
+        // PART2_TICKS value found by watching frame animation
+
+        for (int y = 0; y < map_size.y; y++) {
+            for (int x = 0; x < map_size.y; x++) {
+                boolean found = false;
+                for (Robot r : new_positions) {
+                    if ((r.pos.x == x) && (r.pos.y == y)) {
+                        found = true;
+                        break;
+                    }
+                }
+                if (found) {
+                    out.print('*');
+                } else {
+                    out.print(' ');
+                }
+            }
+            out.println();
+        }
+
 
         long answer = -1;
         return String.valueOf(answer);
