56eaf34af3347f7010c73855c9f647234ed23429
M.A. MacLeay <dnabre@happybunnylabs.com>
2024-12-17 07:44:31 -0700

    Updated main running to have tests and input filenames for rest of year.

--- src/main/java/aoc_2024/AdventOfCode2024.java
+++ src/main/java/aoc_2024/AdventOfCode2024.java
@@ -20,9 +20,16 @@ public class AdventOfCode2024 {
             {"inputs/2024/day_13_test_01.txt"},
             {"inputs/2024/day_14_test_01.txt"},
             {"inputs/2024/day_15_test_01.txt","inputs/2024/day_15_test_02.txt","inputs/2024/day_15_test_03.txt"},
-            {"inputs/2024/day_16_test_01.txt"},
-
-
+            {"inputs/2024/day_16_test_01.txt","inputs/2024/day_16_test_02.txt"},
+            {"inputs/2024/day_17_test_01.txt"},
+            {"inputs/2024/day_18_test_01.txt"},
+            {"inputs/2024/day_19_test_01.txt"},
+            {"inputs/2024/day_20_test_01.txt"},
+            {"inputs/2024/day_21_test_01.txt"},
+            {"inputs/2024/day_22_test_01.txt"},
+            {"inputs/2024/day_23_test_01.txt"},
+            {"inputs/2024/day_24_test_01.txt"},
+            {"inputs/2024/day_25_test_01.txt"}
     };
 
     static final String[] input_files= {
@@ -42,37 +49,47 @@ public class AdventOfCode2024 {
             "inputs/2024/day_14_input_01.txt",
             "inputs/2024/day_15_input_01.txt",
             "inputs/2024/day_16_input_01.txt",
+            "inputs/2024/day_17_input_01.txt",
+            "inputs/2024/day_18_input_01.txt",
+            "inputs/2024/day_19_input_01.txt",
+            "inputs/2024/day_20_input_01.txt",
+            "inputs/2024/day_21_input_01.txt",
+            "inputs/2024/day_22_input_01.txt",
+            "inputs/2024/day_23_input_01.txt",
+            "inputs/2024/day_24_input_01.txt",
+            "inputs/2024/day_25_input_01.txt",
     };
 
 
     public static final boolean TESTING = false;
-    public static final int TEST_IDX=1;
+    public static final int TEST_IDX = 1;
     public static final boolean TIMING = true;
-    public static void main(String[] args){
 
-        int day_number=14;
+    public static void main(String[] args) {
+
+        int day_number = 14;
 
 
         String input_string;
         if (TESTING) {
-            input_string = test_files[day_number-1][TEST_IDX-1];
+            input_string = test_files[day_number - 1][TEST_IDX - 1];
         } else {
-            input_string = input_files[day_number-1];
+            input_string = input_files[day_number - 1];
         }
 
         try {
-            long start,end;
-            if(TIMING) {
-                 start = System.currentTimeMillis();
+            long start, end;
+            if (TIMING) {
+                start = System.currentTimeMillis();
             }
 
             String[] results = Day14.runDay(System.out, input_string);
 
-                System.out.printf("\t\tpart1:\t\t%s\n", results[0]);
-                System.out.printf("\t\tpart2:\t\t%s\n", results[1]);
-            if(TIMING) {
-                 end = System.currentTimeMillis();
-                System.out.printf("\n total time: %d ms\n", end-start);
+            System.out.printf("\t\tpart1:\t\t%s\n", results[0]);
+            System.out.printf("\t\tpart2:\t\t%s\n", results[1]);
+            if (TIMING) {
+                end = System.currentTimeMillis();
+                System.out.printf("\n total time: %d ms\n", end - start);
             }
 
 
