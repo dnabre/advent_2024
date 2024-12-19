822451c28c12d4916b4c8f744960a68bfa85c38d
dnabre <dnabre@happybunnylabs.com>
2024-12-19 10:19:42 -0700

    day 19 part 1&2 working clean up possibly convert recursion to iteration

--- src/main/java/aoc_2024/Day19.java
+++ src/main/java/aoc_2024/Day19.java
@@ -15,11 +15,11 @@ import static java.lang.System.out;
 public class Day19 {
 
         public static final String PART1_ANSWER = "367";
-        public static final String PART2_ANSWER = "-1";
+        public static final String PART2_ANSWER = "724388733465031";
 
 
         private static HashMap<String,Boolean> cache = new HashMap<>();
-
+        private static HashMap<String,Long> ways_cache = new HashMap<>();
         public static String[] runDay(String inputString)  {
             out.println("Advent of Code 2024");
             out.print("\tDay  19");
@@ -32,7 +32,7 @@ public class Day19 {
                 String INPUT = Files.readString(Path.of(inputString));
 
                 answers[0] = getPart1(INPUT);
-                answers[1] = getPart2();
+                answers[1] = getPart2(INPUT);
 
                 if (!AdventOfCode2024.TESTING) {
                     if (!answers[0].equals(PART1_ANSWER)) {
@@ -64,14 +64,6 @@ public class Day19 {
 
 
 
-            out.print("patterns: \n\t");
-            out.println(patterns);
-            out.print("designs: \n\t");
-            out.println(designs);
-            out.println();
-            out.println("designs:");
-
-
             int count=0;
             for(int i=0; i < designs.size(); i++) {
                 String design = designs.get(i);
@@ -89,6 +81,28 @@ public class Day19 {
             return String.valueOf(answer);
         }
 
+    public static String getPart2(String INPUT) {
+            out.println("\n part 2\n");
+        String[] lines = INPUT.split("\n");
+        ArrayList<String> patterns = new ArrayList<>(Arrays.stream(lines[0].split(",")).map(String::trim).toList());
+        ArrayList<String> designs = new ArrayList<>();
+
+        for(int i=2; i < lines.length; i++) {
+            String des = lines[i].trim();
+            designs.add(des);
+        }
+        long total =0;
+        for(int i=0;i< designs.size(); i++){
+            String design = designs.get(i);
+            long count = countWays(design, filterPatterns(design,patterns));
+
+            total += count;
+        }
+        long answer = total;
+        return String.valueOf(answer);
+    }
+
+
         private static ArrayList<String> filterPatterns(String design, ArrayList<String> patterns){
             ArrayList<String> viable = new ArrayList<>();
             for(String pat: patterns) {
@@ -102,11 +116,11 @@ public class Day19 {
         private static Boolean canMake(String design, ArrayList<String> patterns) {
             if(design.isEmpty()) return true;
             if(cache.containsKey(design)){
-                out.printf("hit cache size=%d", cache.size());
+
                 return cache.get(design);
             }
 
-            //ArrayList<String> pats_to_use =  filterPatterns(design, patterns);
+
             ArrayList<String> pats_to_use  = patterns;
 
             for(String pattern: pats_to_use){
@@ -120,9 +134,24 @@ public class Day19 {
 
         }
 
+        private static long countWays(String design,ArrayList<String> patterns){
+            if(!canMake(design,patterns)) {
+                return 0;
+            }
+            if(design.isEmpty()) return 1;
+            long ways =0;
+            if(ways_cache.containsKey(design)) {
+                return ways_cache.get(design);
+            }
 
-        public static String getPart2() {
-            long answer = -1;
-            return String.valueOf(answer);
+            for(String pattern: patterns) {
+                if(design.startsWith(pattern)){
+                    ways+= countWays(design.substring(pattern.length()), patterns);
+                }
+            }
+            ways_cache.put(design,ways);
+
+            return ways;
         }
+
 }
