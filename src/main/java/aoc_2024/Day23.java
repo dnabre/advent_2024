f6f39baa8886374aefce0294c4359d16ec88f44b
dnabre <dnabre@happybunnylabs.com>
2024-12-23 11:18:17 -0700

    Trying yet another definition of a triangle

--- src/main/java/aoc_2024/Day23.java
+++ src/main/java/aoc_2024/Day23.java
@@ -4,10 +4,7 @@ import java.io.IOException;
 import java.io.PrintStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.HashSet;
+import java.util.*;
 
 import static java.lang.System.out;
 
@@ -17,7 +14,8 @@ public class Day23 {
     public static final String PART2_ANSWER = "aq,cc,ea,gc,jo,od,pa,rg,rv,ub,ul,vr,yy";
     private static LinkPair[] linkPairs;
     private static Host[] hosts;
-    private static HashMap<Host, HashSet<Host>> connections;
+    private static HashMap<Host, Node> host_to_node;
+    private static List<Node> node_list;
     private static String[] part1_test_output;
 
     public static String[] runDay(PrintStream out, String inputString) throws IOException {
@@ -46,7 +44,7 @@ public class Day23 {
         return answers;
     }
 
-    record Host(String name) implements Comparable<Host> {
+     record Host(String name)  {
         static Host[] fromStringPair(String s) {
             Host[] ha = new Host[2];
             String[] part = s.split("-");
@@ -55,65 +53,49 @@ public class Day23 {
             return ha;
         }
 
+    }
 
-        @Override
-        public int compareTo(Host other) {
-            return this.name.compareTo(other.name);
-        }
+    record Node(Host name, HashSet<Host> adjacent) {
+    }
 
-        @Override
-        public String toString() {
-            return name;
+
+     static HashMap<Host, Node> getHostToNode() {
+
+        HashSet<Node> node_set = new HashSet<>();
+        HashMap<Host, Node> node_map = new HashMap<>();
+        for (LinkPair lp : linkPairs) {
+            Node node1, node2;
+            if (node_map.containsKey(lp.left)) {
+                node1 = node_map.get(lp.left);
+            } else {
+                node1 = new Node(lp.left, new HashSet<>());
+                node_map.put(lp.left, node1);
+            }
+            if (node_map.containsKey(lp.right)) {
+                node2 = node_map.get(lp.right);
+            } else {
+                node2 = new Node(lp.right, new HashSet<>());
+                node_map.put(lp.right, node2);
+            }
+            node_set.add(node1);
+            node_set.add(node2);
+            node1.adjacent.add(node2.name);
+            node2.adjacent.add(node1.name);
         }
+        node_list = node_set.stream().toList();
+        return node_map;
     }
 
     record LinkPair(Host left, Host right) {
-        public static boolean doesConnect(Host me, Host other) {
-            if (connections.containsKey(me) && connections.containsKey(other)) {
-                HashSet<Host> me_connects = connections.get(me);
-                HashSet<Host> other_connects = connections.get(other);
-                return me_connects.contains(other) && other_connects.contains(me);
-            } else {
-                return false;
-            }
-        }
-
-        public static HashMap<Host, HashSet<Host>> getConnectiontMap() {
-            HashMap<Host, HashSet<Host>> connects = new HashMap<>();
-            for (LinkPair lp : linkPairs) {
-
-                HashSet<Host> set1 = connects.getOrDefault(lp.left, new HashSet<>());
-                set1.add(lp.left);
-                set1.add(lp.right);
-                connects.put(lp.left, set1);
-
-                HashSet<Host> set2 = connects.getOrDefault(lp.right, new HashSet<>());
-                set2.add(lp.left);
-                set2.add(lp.right);
-                connects.put(lp.left, set2);
-
-            }
-            return connects;
-        }
-
         @Override
         public String toString() {
             return String.format("<%s-%s>", left, right);
         }
-
-        public boolean contains(Host h) {
-            return left.equals(h) || right.equals(h);
-        }
-
-        public Host other(Host h) {
-            if (h.equals(left)) {
-                return right;
-            } else {
-                return left;
-            }
-        }
-
     }
+
+
+
+
 
     public static void parseInput(String filename) throws IOException {
         String[] lines = Files.readAllLines(Path.of(filename)).toArray(new String[0]);
@@ -127,17 +109,7 @@ public class Day23 {
             linkPairs[i] = lp;
         }
         hosts = h_set.toArray(new Host[0]);
-        Arrays.sort(hosts);
-        connections = LinkPair.getConnectiontMap();
-
-        if(AdventOfCode2024.TESTING) {
-            String i2_filename = AdventOfCode2024.test_files[23-1][2-1];
-            String[] i_lines =  Files.readAllLines(Path.of(i2_filename)).toArray(new String[0]);
-            part1_test_output = new String[i_lines.length];
-            for(int j=0; j < i_lines.length; j++) {
-                part1_test_output[j] = i_lines[j].trim();
-            }
-        }
+         host_to_node= getHostToNode();
 
 
     }
@@ -147,64 +119,9 @@ public class Day23 {
         out.printf("LinkPairs: %d \n", linkPairs.length);
         out.printf("Hosts    : %d \n", hosts.length);
 
-        HashSet<HashSet<Host>> triangles = new HashSet<>();
-
-        for (int one = 0; one < hosts.length; one++) {
-            for (int two = one + 1; two < hosts.length; two++) {
-                for (int three = two + 1; three < hosts.length; three++) {
-                    if ((one == two) || (two == three) || (one == three)) {
-                        out.printf("ERROR: overlap: %d, %d, %d\n", one, two, three);
-                        return "ERROR: overlap";
-                    }
-                    Host[] tri = {hosts[one], hosts[two], hosts[three]};
-                    if (LinkPair.doesConnect(tri[0], tri[1]) && LinkPair.doesConnect(tri[1], tri[2]) && LinkPair.doesConnect(tri[2], tri[0])) {
-
-                        AddTri(tri, triangles);
-
-                    }
-                }
-            }
+        for(Node node: node_list) {
+            out.println(node);
         }
-        out.printf("triangles: %d\n", triangles.size());
-        out.println();
-        String[] just_for_output = new String[triangles.size()];
-        HashSet<String> test_set = new HashSet<>();
-        int s_idx = 0;
-        for (HashSet<Host> triple : triangles) {
-            String[] tri = new String[3];
-            int idx = 0;
-            for (Host h : triple) {
-                tri[idx] = h.name;
-                idx++;
-            }
-            Arrays.sort(tri);
-            String str = String.format("%s,%s,%s", tri[0], tri[1], tri[2]);
-            just_for_output[s_idx] = str;
-            s_idx++;
-            test_set.add(str);
-        }
-        if(AdventOfCode2024.TESTING) {
-            HashSet<String> expected = new HashSet<String>();
-            for (String s : part1_test_output) {
-                expected.add(s);
-            }
-            Arrays.sort(just_for_output);
-            for(String s: just_for_output) {
-                boolean ok = expected.contains(s);
-                out.println(s);
-                if (!ok) {
-                    out.println("\t ^ bad");
-                }
-            }
-            out.println();
-            for(String g:part1_test_output) {
-                if(!test_set.contains(g)) {
-                    out.printf("missing output: %s\n", g);
-                }
-            }
-        }
-
-
 
 
 
