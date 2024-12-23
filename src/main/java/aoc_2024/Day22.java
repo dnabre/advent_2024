06c9390b9c450b167bd272de3823dbeb5342d8c3
dnabre <dnabre@happybunnylabs.com>
2024-12-23 16:37:14 -0700

    day 22 cleanup after quarter-hearted attemtp  for speed.

--- src/main/java/aoc_2024/Day22.java
+++ src/main/java/aoc_2024/Day22.java
@@ -4,29 +4,22 @@ import java.io.IOException;
 import java.io.PrintStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 
-import static java.lang.System.out;
-
 /***
  * Note: prototyped in python, some oddities in a direct translation
- * 
+ *
  */
 
 public class Day22 {
 
     public static final String PART1_ANSWER = "13753970725";
     public static final String PART2_ANSWER = "1570";
-
-
+    private static final int STEPS = 2000;
     private static long[] start_numbers;
-
-    public record Pair(long left, long right) {
-    }
-
-    public record Quad(long one, long two, long three, long four) {}
+    private static long[] diff;
+    private static long[] diff2;
 
     public static String[] runDay(PrintStream out, String inputString) throws IOException {
         out.println("Advent of Code 2024");
@@ -56,17 +49,18 @@ public class Day22 {
     }
 
     public static void parseInputs(String filename) throws IOException {
-            String[] lines = Files.readAllLines(Path.of(filename)).toArray(new String[0]);
-            start_numbers = new long[lines.length];
-            for(int i=0; i < lines.length; i++) {
-                start_numbers[i] = Long.parseLong(lines[i]);
-            }
+        String[] lines = Files.readAllLines(Path.of(filename)).toArray(new String[0]);
+        start_numbers = new long[lines.length];
+        for (int i = 0; i < lines.length; i++) {
+            start_numbers[i] = Long.parseLong(lines[i]);
+        }
     }
+
     public static String getPart1() {
         long total = 0L;
-        for(long secret: start_numbers) {
-            for(int i=0; i < 2000; i++) {
-               secret = step(secret);
+        for (long secret : start_numbers) {
+            for (int i = 0; i < 2000; i++) {
+                secret = step(secret);
             }
             total += secret;
         }
@@ -89,8 +83,6 @@ public class Day22 {
 
     }
 
-    private static long diff[];
-    private static long diff2[];
     private static void getDiffs(long x) {
         //Pair[] diffs = new Pair[STEPS];
         diff = new long[STEPS];
@@ -100,36 +92,29 @@ public class Day22 {
         long b;
         long not_b;
 
-        for(int i=0; i < 2000; i++) {
+        for (int i = 0; i < 2000; i++) {
             current_n = step(last);
             b = last % 10;
             not_b = current_n % 10;
             last = current_n;
-            //diffs[i]= new Pair(not_b-b, not_b);
-            diff[i] = not_b-b;
+            diff[i] = not_b - b;
             diff2[i] = not_b;
         }
-        //return diffs;
     }
 
-private static final int STEPS=2000;
     public static String getPart2() {
 
-        HashMap<Quad,Long> daily = new HashMap<>();
-        for(long init:start_numbers ) {
+        HashMap<Quad, Long> daily = new HashMap<>();
+        for (long init : start_numbers) {
             getDiffs(init);
             HashSet<Quad> added = new HashSet<>();
             for (int j = 0; j < STEPS - 4; j++) {
 
-                Quad each_day = new Quad(
-                        diff[j+0],
-                        diff[j+1],
-                        diff[j+2],
-                        diff[j+3]);
+                Quad each_day = new Quad(diff[j], diff[j + 1], diff[j + 2], diff[j + 3]);
 
                 if (!added.contains(each_day)) {
                     long offset = daily.getOrDefault(each_day, 0L);
-                    offset += diff2[j+3];
+                    offset += diff2[j + 3];
                     daily.put(each_day, offset);
                     added.add(each_day);
                 }
@@ -137,11 +122,14 @@ private static final int STEPS=2000;
         }
 
         long max = Long.MIN_VALUE;
-        for(long t:daily.values()) {
+        for (long t : daily.values()) {
             max = Math.max(max, t);
         }
 
         long answer = max;
         return String.valueOf(answer);
+    }
+
+    public record Quad(long one, long two, long three, long four) {
     }
 }
