83d07c6524e02cfe2954183887eb2c8a90c9d78c
dnabre <dnabre@happybunnylabs.com>
2025-01-15 15:09:50 -0700

    Setup to tackle Day 24

--- src/main/java/aoc_2024/Day24.java
+++ src/main/java/aoc_2024/Day24.java
@@ -42,92 +42,23 @@ public class Day24 {
         return answers;
     }
 
-    enum BIN_OP {
-        AND, OR, XOR
-    }
+    protected static String getPart1() {
 
-    enum trit {
-        TRUE, FALSE, UNKNOWN;
-
-        @Override
-        public String toString() {
-           return switch(this) {
-                case TRUE -> "1";
-                case FALSE -> "0";
-                case UNKNOWN -> "?";
-            };
-        }
-    }
-
-    private sealed interface Gate {
-        trit getValue();
-    }
-    private record ConstantGate(trit v) implements Gate{
-        @Override
-        public trit getValue() {
-            return trit.UNKNOWN;
-        }
-    }
-    private record AndGate(Gate left, Gate right) implements  Gate{
-        @Override
-        public trit getValue() {
-            return trit.UNKNOWN;
-        }
-    }
-    private record ORGate(Gate left, Gate right) implements Gate{
-        @Override
-        public trit getValue() {
-            return trit.UNKNOWN;
-        }
-    }
-    private record XORGate(Gate left, Gate right) implements Gate{
-        @Override
-        public trit getValue() {
-            return trit.UNKNOWN;
-        }
-    }
-
-
-    private static void parseInput(String filename) throws IOException {
-        String[] lines = Files.readAllLines(Path.of(filename)).toArray(new String[0]);
-        out.printf("read %d lines\n", lines.length);
-        ArrayList<Gate> gate_list = new ArrayList<>();
-        HashMap<String,Gate> name_map = new HashMap<>();
-        ArrayList<ConstantGate> input_gates = new ArrayList<>();
-
-        int break_idx=-1;
-        for(int i=0; i<lines.length; i++) {
-            if(lines[i].equals("")){
-                break_idx= i+1;
-                break;
-            }
-            out.printf("%3d:\t%s\n", i, lines[i]);
-            String[] parts = lines[i].split(":");
-            String gate_name = parts[0].trim();
-            trit value = parts[1].trim().equals("1")?trit.TRUE:trit.FALSE;
-            ConstantGate cg = new ConstantGate(value);
-
-
-        }
-        for(int i=break_idx; i < lines.length; i++) {
-            out.printf("%3d:\t%s\n", i, lines[i]);
-        }
-
-
-        out.printf("break idx: %d\n", break_idx);
-
-
-    }
-
-
-    public static String getPart1() {
         long answer = -1;
         return String.valueOf(answer);
     }
 
+    protected static String getPart2() {
 
-    public static String getPart2() {
         long answer = -1;
         return String.valueOf(answer);
     }
+
+    protected static void parseInput(String filename) throws IOException {
+
+    }
+
+
+
+
 }
